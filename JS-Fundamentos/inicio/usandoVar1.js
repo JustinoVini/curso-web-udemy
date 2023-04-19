b9ed891950474e7c177315872e7322831b506861
Vinicius Justino <visilva5690@hotmail.com>
2023-04-18 22:19:43 -0300

    push master

---
+++ JS-Fundamentos/inicio/usandoVar1.js
@@ -0,0 +1,18 @@
+{
+    {
+        {
+            {
+                let sera = 'Será???'
+                console.log(sera)
+            }
+        }
+    }
+}
+console.log(sera);
+
+function teste(){
+    let local = 123
+    console.log(local);
+}
+
+teste()
